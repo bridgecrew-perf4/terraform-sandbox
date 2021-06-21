1c2a66881a5dd4ef12523ff8821b4c17df926b9b
Sadayoshi Tada <tadainfra@gmail.com>
2021-06-21 23:56:02 +0900

    install.sh修正

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,4 +6,4 @@ DOWNLOAD_URL="${BASE_URL}/v0.7.0/tfnotify_linux_amd64.tar.gz"
 
 wget ${DOWNLOAD_URL} -P /tmp
 tar zxvf /tmp/tfnotify_linux_amd64.tar.gz -C /tmp
-mv /tmp/tfnotify_linux_amd64.tar.gz/tfnotify /usr/local/bin/tfnotify
+mv /tmp/tfnotify_linux_amd64/tfnotify /usr/local/bin/tfnotify
