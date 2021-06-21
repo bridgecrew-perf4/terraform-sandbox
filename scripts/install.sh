40ceb88507017da01409f235cad31a8e4593dd03
Sadayoshi Tada <tadainfra@gmail.com>
2021-06-21 18:25:41 +0900

    tfnotifyのバージョン修正

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 
 BASE_URL=https://github.com/mercari/tfnotify/release/download
 
-DOWNLOAD_URL="${BASE_URL}/v0.6.1/tfnotify_linux_amd64.tar.gz"
+DOWNLOAD_URL="${BASE_URL}/v0.3.1/tfnotify_linux_amd64.tar.gz"
 
 wget ${DOWNLOAD_URL} -P /tmp
-tar zxvf /tmp/tfnotify_v0.6.1_linux_amd64.tar.gz -C /tmp
-mv /tmp/tfnotify_v0.6.1_linux_amd64/tfnotify /usr/local/bin/tfnotify
+tar zxvf /tmp/tfnotify_v0.3.1_linux_amd64.tar.gz -C /tmp
+mv /tmp/tfnotify_v0.3.1_linux_amd64/tfnotify /usr/local/bin/tfnotify
