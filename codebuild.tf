20912514ce829118b846eaac896eeaa2a158bcce
Sadayoshi Tada <tadainfra@gmail.com>
2021-07-02 19:47:44 +0900

    修正

--- codebuild.tf
+++ codebuild.tf
@@ -1,5 +1,5 @@
 module "continuous_apply_codebuild_role" {
-  source     = "../module/iam"
+  source     = "./module/iam"
   name       = "continuous-apply"
   identifier = "codebuild.amazonaws.com"
   policy     = data.aws_iam_policy.administrator_access.policy
