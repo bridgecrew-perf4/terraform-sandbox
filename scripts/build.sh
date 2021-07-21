4e41e6ec13197304924109f9d3dee6ce6a7dcb29
Sadayoshi Tada <tadainfra@gmail.com>
2021-07-22 04:59:05 +0900

    コマンド修正

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-if [[ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/main' ]]; then
+if [ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/main' ]; then
   ${CODEBUILD_SRC_DIR}/scripts/apply.sh main
-elif [[ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/develop' ]]; then
+elif [ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/develop' ]; then
   ${CODEBUILD_SRC_DIR}/scripts/apply.sh develop
 else
   ${CODEBUILD_SRC_DIR}/scripts/plan.sh
