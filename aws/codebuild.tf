6a22c08e3237f7e8ab209e8419976623dc8cdcfa
Sadayoshi Tada <tadainfra@gmail.com>
2021-07-24 17:44:17 +0900

    ディレクトリ名変更に伴い移動

---
+++ aws/codebuild.tf
@@ -0,0 +1,87 @@
+module "continuous_apply_codebuild_role" {
+  source     = "./module/iam"
+  name       = "tag-release-test-role"
+  identifier = "codebuild.amazonaws.com"
+  policy     = data.aws_iam_policy.administrator_access.policy
+}
+
+data "aws_iam_policy" "administrator_access" {
+  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_codebuild_project" "tag_release_build" {
+  name         = "tag-release-test"
+  service_role = module.continuous_apply_codebuild_role.iam_role_arn
+
+  source {
+    type      = "GITHUB"
+    location  = "https://github.com/tasogare0919/apprunner-express-app"
+    buildspec = "buildspec.yml"
+  }
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    type            = "LINUX_CONTAINER"
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:3.0"
+    privileged_mode = true
+
+    environment_variable {
+      name  = "AWS_DEFAULT_REGION"
+      value = var.region
+    }
+
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = var.account
+    }
+
+    environment_variable {
+      name  = "IMAGE_REPO_NAME"
+      value = "simple-express-repository"
+    }
+
+  }
+
+
+}
+
+# resource "aws_codebuild_webhook" "tag_release_build_webhook" {
+#   project_name = aws_codebuild_project.tag_release_build.name
+
+#   filter_group {
+#     filter {
+#       type    = "EVENT"
+#       pattern = "PULL_REQUEST_CREATED"
+#     }
+#   }
+
+#   filter_group {
+#     filter {
+#       type    = "EVENT"
+#       pattern = "PULL_REQUEST_UPDATED"
+#     }
+#   }
+
+#   filter_group {
+#     filter {
+#       type    = "EVENT"
+#       pattern = "PULL_REQUEST_REOPENED"
+#     }
+#   }
+
+#   filter_group {
+#     filter {
+#       type    = "EVENT"
+#       pattern = "PUSH"
+#     }
+
+#     filter {
+#       type    = "HEAD_REF"
+#       pattern = "^refs/tags/v*"
+#     }
+#   }
+# }
