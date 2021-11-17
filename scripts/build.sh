47cdb7ed0fedfa1cd3f38ded3c972673a0d8c287
Sadayoshi Tada <tadainfra@gmail.com>
2021-11-17 15:07:54 +0900

    スクリプト回収

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ set -x
 
 if [ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/main' ]; then
   ${CODEBUILD_SRC_DIR}/scripts/apply.sh main
-elif [ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/develop' ]; then
-  ${CODEBUILD_SRC_DIR}/scripts/apply.sh develop
+# elif [ ${CODEBUILD_WEBHOOK_TRIGGER} = 'branch/develop' ]; then
+#   ${CODEBUILD_SRC_DIR}/scripts/apply.sh develop
 else
   ${CODEBUILD_SRC_DIR}/scripts/plan.sh
 fi
--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-cd ${CODEBUILD_SRC_DIR}/aws
-terraform init -input=false -no-color
-terraform plan -input=false -no-color | \
-tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$(date)"
+TARGET_BRANCH=main
+MERGE_BASE=`git show-branch --merge-base origin/${TARGET_BRANCH} HEAD`
+
+
+for FILE in $(git diff ${MERGE_BASE} HEAD --diff-filter=AM --name-only -- "*.tf") ; do
+    cd ${CODEBUILD_SRC_DIR}/aws
+    RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
+    if [ -n "$RES" ]; then
+        terraform init -input=false -no-color
+        terraform plan -input=false -no-color | \
+        tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$(date)
+    elif [ -z "$RES" ]; then
+        echo "tf file nothing"
+        cd ${CODEBUILD_SRC_DIR}
+    fi
+done
