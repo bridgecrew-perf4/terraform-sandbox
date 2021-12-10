3ba561802e7b9abc4b2e6cecb61b266ee1f77cb1
Sadayoshi Tada <tadainfra@gmail.com>
2021-12-10 11:37:15 +0900

    エラー修正

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -5,11 +5,11 @@ cd ${CODEBUILD_SRC_DIR}/aws
 CURRENT_DIR=`pwd`
 for FILE in $(git diff HEAD^..HEAD --diff-filter=AM --name-only -- "*.tf") ; do
   cd $(dirname ${FILE}); cd ../
-  RES=`\find . -name "*.yaml" -maxdepth 1 2> /dev/null`
+  RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
   if [ -n "$RES" ]; then
     terraform init -input=false -no-color
     terraform plan -input=false -no-color | \
-    tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$(date)
+    tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$(date)"
     cd $CURRENT_DIR
   elif [ -z "$RES" ]; then
     echo "tf files nothing"
