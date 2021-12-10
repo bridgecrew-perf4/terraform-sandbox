532d7eeeeaade658786292ce37ad305a51ac6e10
Sadayoshi Tada <tadainfra@gmail.com>
2021-12-10 11:33:40 +0900

    エラー修正

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -16,15 +16,3 @@ for FILE in $(git diff HEAD^..HEAD --diff-filter=AM --name-only -- "*.tf") ; do
     cd $CURRENT_DIR
   fi
 done
-
-# for FILE in $(git diff ${MERGE_BASE} HEAD --diff-filter=AM --name-only -- "*.tf") ; do
-#     RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
-#     if [ -n "$RES" ]; then
-#         terraform init -input=false -no-color
-#         terraform plan -input=false -no-color | \
-#         tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$(date)
-#     elif [ -z "$RES" ]; then
-#         echo "tf file nothing"
-#         cd ${CODEBUILD_SRC_DIR}
-#     fi
-# done
