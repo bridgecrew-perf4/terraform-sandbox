4e3565c4a80839696689761ee2bda461325c9bc8
Sadayoshi Tada <tadainfra@gmail.com>
2021-12-10 11:25:42 +0900

    スクリプト修正

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 
-TARGET_BRANCH=main
-MERGE_BASE=`git show-branch --merge-base origin/${TARGET_BRANCH} HEAD`
-
 cd ${CODEBUILD_SRC_DIR}/aws
 
-for FILE in $(git diff ${MERGE_BASE} HEAD --diff-filter=AM --name-only -- "*.tf") ; do
-    RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
-    if [ -n "$RES" ]; then
-        terraform init -input=false -no-color
-        terraform plan -input=false -no-color | \
-        tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$(date)
-    elif [ -z "$RES" ]; then
-        echo "tf file nothing"
-        cd ${CODEBUILD_SRC_DIR}
-    fi
+CURRENT_DIR=`pwd`
+for FILE in $(git diff HEAD^..HEAD --diff-filter=AM --name-only -- "*.tf") ; do
+  cd $(dirname ${FILE}); cd ../
+  RES=`\find . -name "*.yaml" -maxdepth 1 2> /dev/null`
+  if [ -n "$RES" ]; then
+    terraform init -input=false -no-color
+    terraform plan -input=false -no-color | \
+    tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$(date)
+    cd $CURRENT_DIR
+  elif [ -z "$RES" ]; then
+    echo "tf files nothing"
+    cd $CURRENT_DIR
+  fi
 done
+
+# for FILE in $(git diff ${MERGE_BASE} HEAD --diff-filter=AM --name-only -- "*.tf") ; do
+#     RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
+#     if [ -n "$RES" ]; then
+#         terraform init -input=false -no-color
+#         terraform plan -input=false -no-color | \
+#         tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml plan --message "$(date)
+#     elif [ -z "$RES" ]; then
+#         echo "tf file nothing"
+#         cd ${CODEBUILD_SRC_DIR}
+#     fi
+# done
