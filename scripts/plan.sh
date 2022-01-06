8736345650e5738c990e9f069223b7a91ff21c99
Sadayoshi Tada <tadainfra@gmail.com>
2022-01-06 10:34:24 +0900

    コマンド修正

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CURRENT_DIR=`pwd`
 for FILE in $(git diff origin/main --diff-filter=AM --name-only -- "*.tf" --relative –no-prefix aws) ; do
-  cd $(dirname ${FILE}); cd ../
+  cd $(dirname ${FILE})
   RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
   if [ -n "$RES" ]; then
     terraform init -input=false -no-color
