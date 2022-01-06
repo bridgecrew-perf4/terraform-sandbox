a8ce44120328c070e5b8d8a68254d0b66cfa2ae2
Sadayoshi Tada <tadainfra@gmail.com>
2022-01-06 11:53:37 +0900

    すくりぷとしゅうせい　

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+CURRENT_DIR=`pwd`
 MESSAGE=$(git log ${CODEBUILD_SOURCE_VERSION} -1 --pretty=format:"%s")
 CODEBUILD_SOURCE_VERSION=$(echo ${MESSAGE} | cut -f4 -d' ' | sed 's/#/pr\//')
-cd ${CODEBUILD_SRC_DIR}/aws
-terraform init -input=false -no-color
-terraform apply -input=false -no-color -auto-approve | \
-tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml apply --message "$(date)"
+
+# terraform init -input=false -no-color
+# terraform apply -input=false -no-color -auto-approve | \
+# tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml apply --message "$(date)"
+
+for FILE in $(git diff origin/main --diff-filter=AM --name-only -- "*.tf" --relative –no-prefix aws) ; do
+  cd $(dirname ${FILE})
+  RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
+  if [ -n "$RES" ]; then
+    terraform init -input=false -no-color
+    terraform apply -input=false -no-color -auto-approve | \
+    tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml apply --message "$(date)"
+    cd $CURRENT_DIR
+  elif [ -z "$RES" ]; then
+    echo "tf files nothing"
+    cd $CURRENT_DIR
+  fi
+done
