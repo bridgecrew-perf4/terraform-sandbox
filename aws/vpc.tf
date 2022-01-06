0aedc3390b31a01df7cf9fd32d6ff91ec84a4368
Sadayoshi Tada <tadainfra@gmail.com>
2022-01-06 10:37:14 +0900

    エラー箇所削除

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -94,21 +94,3 @@ resource "aws_route_table_association" "public_1c" {
   subnet_id      = aws_subnet.tf_public_subnet_1c.id
   route_table_id = aws_route_table.public.id
 }
-
-resource "aws_route_table" "private_1a" {
-  vpc_id = aws_vpc.tf_vpc.id
-
-  tags = {
-    Name = "tf-private-1a"
-  }
-}
-
-# resource "aws_route" "private_1a" {
-#   route_table_id         = aws_route_table.private_1a.id
-# }
-
-resource "aws_vpc_endpoint_route_table_association" "private_1a" {
-  vpc_endpoint_id         = aws_vpc_endpoint.s3.id
-  route_table_id = aws_route_table.private_1a.id
-}
-
