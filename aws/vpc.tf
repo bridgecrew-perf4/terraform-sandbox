0c1ceb72f15585e045ccffb6a0ee173a9a56a35c
Sadayoshi Tada <tadainfra@gmail.com>
2022-01-06 10:32:53 +0900

    差分だし

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -36,17 +36,17 @@ resource "aws_subnet" "tf_public_subnet_1a" {
   }
 }
 
-resource "aws_subnet" "tf_public_subnet_1d" {
-  vpc_id                  = aws_vpc.tf_vpc.id
-  cidr_block              = "10.10.5.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-northeast-1d"
+# resource "aws_subnet" "tf_public_subnet_1d" {
+#   vpc_id                  = aws_vpc.tf_vpc.id
+#   cidr_block              = "10.10.5.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = "ap-northeast-1d"
 
-  tags = {
-    Name  = "tf-public-subnet-1d"
-    Owner = "tada"
-  }
-}
+#   tags = {
+#     Name  = "tf-public-subnet-1d"
+#     Owner = "tada"
+#   }
+# }
 
 resource "aws_subnet" "tf_private_subnet_1a" {
   vpc_id            = aws_vpc.tf_vpc.id
@@ -68,5 +68,47 @@ resource "aws_subnet" "tf_private_subnet_1c" {
     Name  = "tf-private-subnet-1c"
     Owner = "tada"
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  tags = {
+    Name = "tf-public"
+  }
+}
+
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = "igw-0e1154ca948ec3011"
+}
+
+# Association
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = aws_subnet.tf_public_subnet_1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1c" {
+  subnet_id      = aws_subnet.tf_public_subnet_1c.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_1a" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  tags = {
+    Name = "tf-private-1a"
+  }
+}
+
+# resource "aws_route" "private_1a" {
+#   route_table_id         = aws_route_table.private_1a.id
+# }
+
+resource "aws_vpc_endpoint_route_table_association" "private_1a" {
+  vpc_endpoint_id         = aws_vpc_endpoint.s3.id
+  route_table_id = aws_route_table.private_1a.id
 }
 
