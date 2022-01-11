099011319fbe7c312aac232ca2edae1972855602
Sadayoshi Tada <tadainfra@gmail.com>
2022-01-11 11:29:27 +0900

    スクリプト修正

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -4,18 +4,13 @@ CURRENT_DIR=`pwd`
 MESSAGE=$(git log ${CODEBUILD_SOURCE_VERSION} -1 --pretty=format:"%s")
 CODEBUILD_SOURCE_VERSION=$(echo ${MESSAGE} | cut -f4 -d' ' | sed 's/#/pr\//')
 
-# terraform init -input=false -no-color
-# terraform apply -input=false -no-color -auto-approve | \
-# tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml apply --message "$(date)"
-
-for FILE in $(git diff HEAD^..HEAD --diff-filter=AM --name-only -- "*.tf" --relative –no-prefix aws) ; do
-  cd $(dirname ${FILE})
-  RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
-  if [ -n "$RES" ]; then
-    terraform init -input=false -no-color
-    terraform apply -input=false -no-color -auto-approve | \
-    tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml apply --message "$(date)"
-  elif [ -z "$RES" ]; then
-    echo "tf files nothing"
-  fi
-done
+DIFF_FILES=$(git diff HEAD^..HEAD --diff-filter=AM --name-only -- "*.tf" --relative –no-prefix aws)
+cd $(dirname ${DIFF_FILES})
+RES=`\find . -name "*.tf" -maxdepth 1 2> /dev/null`
+if [ -n "$RES" ]; then
+  terraform init -input=false -no-color
+  terraform apply -input=false -no-color -auto-approve | \
+  tfnotify --config ${CODEBUILD_SRC_DIR}/tfnotify.yml apply --message "$(date)"
+elif [ -z "$RES" ]; then
+  echo "tf files nothing"
+fi
